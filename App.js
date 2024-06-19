54684237b30755206f31abd701444459a01eb36f
Hamdan Patel <hpatel164@hawk.iit.edu>
2024-06-19 10:07:39 +0530

    Added logo

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 
+const Logo = () => {
+    return <img src="https://img.icons8.com/?size=100&id=dcNXeTC0SjGX&format=png&color=000000" id="logo"/>
+}
+
+const SearchBar = () => {
+
+}
+
+const UserIcon = () => {
+
+}
 
 const NavBar = () => {
-    (
-        <div>
+    return (
+        <div id="NavBar">
             <Logo />
             <SearchBar />
             <UserIcon />
         </div>
-    )
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<NavBar />)
