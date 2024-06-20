cd9e621684b058e18b160baa5619e59756c29246
Hamdan Patel <hpatel164@hawk.iit.edu>
2024-06-20 11:34:30 +0530

    Added id's to html tags

--- App.js
+++ App.js
@@ -2,15 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 const Logo = () => {
-    return <img src="https://img.icons8.com/?size=100&id=dcNXeTC0SjGX&format=png&color=000000" id="logo" />
+    return <div><img src="https://img.icons8.com/?size=100&id=dcNXeTC0SjGX&format=png&color=000000" id="logo" /></div>
 }
 
 const SearchBar = () => {
-
+    return <div><input type="text" placeholder="Search..." id="searchBar" /></div>
 }
 
 const UserIcon = () => {
-    return <img src="https://img.icons8.com/?size=100&id=2yC9SZKcXDdX&format=png&color=000000" id="user-icon" />
+    return <div><img src="https://img.icons8.com/?size=100&id=2yC9SZKcXDdX&format=png&color=000000" id="user-icon" /></div>
 }
 
 const NavBar = () => {
